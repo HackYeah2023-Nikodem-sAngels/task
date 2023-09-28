3422c693df544b4730101c40986c5726c3bba1bd
Plasny <git.plasny.uq95y@slmail.me>
2023-09-28 20:34:47 +0200

    mend

---
+++ README.md
@@ -0,0 +1,10 @@
+# Project for HackYeah 2023
+
+## About us
+
+We are a group of friends from [Upper-secondary Schools of Communications](https://tl.krakow.pl/).
+
+## About project 
+
+Todo
+
---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Hackyeah project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        dev-go-script = pkgs.writeShellScriptBin "dev-back"
+          ''
+            PROJECT_DIR=$(echo "$DIRENV_DIR" | cut -c 2-)
+            cd "$PROJECT_DIR/backend" || return
+            reflex -r '\.go$' -s -- go run .
+          '';
+
+        dev-front-script = pkgs.writeShellScriptBin "dev-front"
+          ''
+            PROJECT_DIR=$(echo "$DIRENV_DIR" | cut -c 2-)
+            cd "$PROJECT_DIR/frontend" || return
+            bun --watch run start
+          '';
+      in
+      {
+        check = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              shellcheck.enable = true;
+              eslint.enable = true;
+              gofmt.enable = true;
+            };
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          shellHook = ''
+            ${self.check.${system}.pre-commit-check.shellHook}
+
+            printf "\e[33m
+            Let's begin coding 😎
+            run \e[1mdev-back\e[0m\e[33m -> to develop backend
+            run \e[1mdev-front\e[0m\e[33m -> to develop frontend
+            \e[0m
+            "
+          '';
+
+          buildInputs = with pkgs; [
+            bun
+            reflex
+            go
+          ] ++ [
+            dev-go-script
+            dev-front-script
+          ];
+        };
+      }
+    );
+}
