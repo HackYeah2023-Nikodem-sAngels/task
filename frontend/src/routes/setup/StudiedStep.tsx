f2623b00256f60ccd8f13be774b590ed70786449
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-10-01 01:17:04 +0200

    Added card around steos

--- frontend/src/routes/setup/StudiedStep.tsx
+++ frontend/src/routes/setup/StudiedStep.tsx
@@ -4,43 +4,46 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import { StepProps } from "../Setup";
 import { useState } from "react";
+import { Card } from "@/components/ui/card";
 
 export function StudiedStep(props: StepProps) {
     const [studied, setStudied] = useState("no");
 
     return (
-        <form
-            className="flex flex-col gap-4"
-            onSubmit={(e) => {
-                e.preventDefault();
-                props.onSubmit({
-                    ...props.data,
-                    studyLevel: studied === "no" ? "1" : undefined,
-                });
-            }}
-        >
-            <h1 className="text-center text-xl font-medium">
-                Czy już studiowałeś/aś?
-            </h1>
-            <RadioGroup
-                defaultValue={studied}
-                value={studied}
-                onValueChange={setStudied}
+        <Card className="h-fit p-8">
+            <form
+                className="flex flex-col gap-4"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    props.onSubmit({
+                        ...props.data,
+                        studyLevel: studied === "no" ? "1" : undefined,
+                    });
+                }}
             >
-                <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="yes" id="yes" />
-                    <Label htmlFor="yes">Tak</Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="no" id="no" />
-                    <Label htmlFor="no">Nie</Label>
-                </div>
-            </RadioGroup>
+                <h1 className="text-center text-xl font-medium">
+                    Czy już studiowałeś/aś?
+                </h1>
+                <RadioGroup
+                    defaultValue={studied}
+                    value={studied}
+                    onValueChange={setStudied}
+                >
+                    <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="yes" id="yes" />
+                        <Label htmlFor="yes">Tak</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="no" id="no" />
+                        <Label htmlFor="no">Nie</Label>
+                    </div>
+                </RadioGroup>
 
-            <Button className="ml-auto flex gap-2" type="submit">
-                Dalej
-                <ArrowRightIcon className="w-4" />
-            </Button>
-        </form>
+                <Button className="ml-auto flex gap-2" type="submit">
+                    Dalej
+                    <ArrowRightIcon className="w-4" />
+                </Button>
+            </form>
+        </Card>
     );
 }
--- frontend/src/routes/setup/StudyLevelStep.tsx
+++ frontend/src/routes/setup/StudyLevelStep.tsx
@@ -4,51 +4,54 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import { StepProps } from "../Setup";
 import { useState } from "react";
+import { Card } from "@/components/ui/card";
 
 export function StudyLevelStep(props: StepProps) {
     const [studyLevel, setStudyLevel] = useState("1");
 
     return (
-        <form
-            className="flex flex-col gap-4"
-            onSubmit={(e) => {
-                e.preventDefault();
-                props.onSubmit({
-                    ...props.data,
-                    studyLevel: studyLevel as "1" | "2" | "3" | "1+2",
-                });
-            }}
-        >
-            <h1 className="text-center text-xl font-medium">
-                Na jakie studia się wybierasz?
-            </h1>
-            <RadioGroup
-                defaultValue={studyLevel}
-                value={studyLevel}
-                onValueChange={setStudyLevel}
+        <Card className="h-fit p-8">
+            <form
+                className="flex flex-col gap-4"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    props.onSubmit({
+                        ...props.data,
+                        studyLevel: studyLevel as "1" | "2" | "3" | "1+2",
+                    });
+                }}
             >
-                <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="1" id="1" />
-                    <Label htmlFor="1">1. stopnia</Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="2" id="2" />
-                    <Label htmlFor="2">2. stopnia</Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="1+2" id="1+2" />
-                    <Label htmlFor="1+2">Jednolite magisterskie</Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="3" id="3" />
-                    <Label htmlFor="1+2">Doktorackie</Label>
-                </div>
-            </RadioGroup>
+                <h1 className="text-center text-xl font-medium">
+                    Na jakie studia się wybierasz?
+                </h1>
+                <RadioGroup
+                    defaultValue={studyLevel}
+                    value={studyLevel}
+                    onValueChange={setStudyLevel}
+                >
+                    <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="1" id="1" />
+                        <Label htmlFor="1">1. stopnia</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="2" id="2" />
+                        <Label htmlFor="2">2. stopnia</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="1+2" id="1+2" />
+                        <Label htmlFor="1+2">Jednolite magisterskie</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="3" id="3" />
+                        <Label htmlFor="1+2">Doktorackie</Label>
+                    </div>
+                </RadioGroup>
 
-            <Button className="ml-auto flex gap-2" type="submit">
-                Dalej
-                <ArrowRightIcon className="w-4" />
-            </Button>
-        </form>
+                <Button className="ml-auto flex gap-2" type="submit">
+                    Dalej
+                    <ArrowRightIcon className="w-4" />
+                </Button>
+            </form>
+        </Card>
     );
 }
