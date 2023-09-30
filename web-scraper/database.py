2fb838ebc7997c58d0e26e65b43f4379ff74a5ad
imarek <izabelamarek2004@gmail.com>
2023-09-30 14:46:43 +0200

    added database

--- web-scraper/database.py
+++ web-scraper/database.py
@@ -1,15 +1,9 @@
 import psycopg2
 
 conn = psycopg2.connect(
-   database="hackyeah", 
-   user='u1', 
-   password='2oxREaU8tmqUANruiQDS62tWJtsBPYW82xsfgY6w', 
-   host='192.168.72.190', 
-   port='5432'
+   database="hackyeah", user='u1', password='2oxREaU8tmqUANruiQDS62tWJtsBPYW82xsfgY6w', host='192.168.72.190', port='5432'
 )
 
 cursor = conn.cursor()
-
-# whatever you want to do
 
 conn.close()
