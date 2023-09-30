1b635fb32f45288e2237d606001768a58167d99e
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-09-30 14:37:09 +0200

    Moved chat to its own route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,5 @@
-import { Prompt } from "@/components/Prompt";
-import { Responses } from "@/components/Responses";
-import { Card } from "@/components/ui/card";
-
 function App() {
-    return (
-        <Card className="flex flex-col h-full mx-auto w-screen p-8">
-            <Responses
-                responses={[
-                    "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
-                    "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
-                ]}
-            />
-            <Prompt />
-        </Card>
-    );
+    return "routes here";
 }
 
 export default App;
---
+++ frontend/src/routes/Chat.tsx
@@ -0,0 +1,17 @@
+import { Prompt } from "@/components/Prompt";
+import { Responses } from "@/components/Responses";
+import { Card } from "@/components/ui/card";
+
+export function Chat() {
+    return (
+        <Card className="flex flex-col h-full mx-auto w-screen p-8">
+            <Responses
+                responses={[
+                    "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+                    "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+                ]}
+            />
+            <Prompt />
+        </Card>
+    );
+}
