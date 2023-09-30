d336cecfc7ea27f41b25278d5b08d60e4d225804
git.nix <nikodemmarekit@gmail.com>
2023-09-30 13:19:34 +0200

    feat(database): database schema

---
+++ database/create_schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE universities (
+    id int,
+
+    name text,
+    description text,
+
+    country text,
+    region text,
+    city text,
+)
+
+CREATE TABLE majors (
+  id int,
+  university_id int,
+
+  name text,
+  description text,
+
+  ECTS int NULL,
+  points_to_qualify int,
+)
+
+CREATE TABLE subjects_to_majors (
+  major_id int,
+  subject_id int,
+)
+
+CREATE TABLE subject (
+  id int,
+
+  name text,
+  description text,
+
+  number_of_lectures int,
+)
