43acdb403829948e61ebc1c4582b839c43006853
AreczeQ192 <askupien8@gmail.com>
2023-09-30 20:03:37 +0200

    language localization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,30 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+    RouterProvider,
+    createBrowserRouter,
+    Navigate,
+} from "react-router-dom";
 import { Form } from "./routes/Form";
 import { Homepage } from "./routes/Homepage";
 import { Chat } from "./routes/Chat";
 
 function App() {
+    const language = Intl.DateTimeFormat().resolvedOptions().locale.slice(0, 2);
+
     const router = createBrowserRouter([
         {
             path: "/",
+            element: <Navigate to={`/${language}`} />,
+        },
+        {
+            path: "/:language",
             element: <Homepage />,
         },
         {
-            path: "/map",
+            path: "/:language/map",
             element: <Form />,
         },
         {
-            path: "/chat",
+            path: "/:language/chat",
             element: <Chat />,
         },
     ]);
--- frontend/src/routes/Homepage.tsx
+++ frontend/src/routes/Homepage.tsx
@@ -1,7 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router";
 
 export function Homepage() {
+    const { language } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!["pl", "en"].includes(language!)) navigate("/en");
+    }, []);
+
     return (
         <div className="mx-auto flex flex-col items-center gap-12 ">
             <h1 className="mt-32 scroll-m-20 text-center text-6xl font-bold leading-tight tracking-tighter ">
