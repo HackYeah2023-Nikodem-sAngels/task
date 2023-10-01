8637df163d5c8d857e9d6a8ce4e6f8d95b4590cb
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-10-01 03:48:25 +0200

    Improved home page

--- frontend/src/routes/Homepage.tsx
+++ frontend/src/routes/Homepage.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { ArrowPathIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
 import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
@@ -20,20 +20,37 @@ export function Homepage() {
     }, []);
 
     return (
-        <div className="mx-auto flex flex-col items-center gap-12 ">
-            <h1 className="mt-32 scroll-m-20 text-center text-6xl font-bold leading-tight tracking-tighter ">
-                <span>Znajdź</span>{" "}
-                <span className="text-primary">uczelnię</span>{" "}
-                <span>dla siebie</span>
-            </h1>
-            <Button
-                size={"lg"}
-                className="flex gap-2 text-lg font-medium"
-                onClick={() => navigate(`/${language}/setup`)}
-            >
-                {authed ? "Kontynuuj" : "Rozpocznij"}
-                <ArrowRightIcon className="w-5" />
-            </Button>
+        <div className="flex h-full items-center">
+            <div className="mx-auto flex flex-col items-center gap-10">
+                <h1 className="text-center text-6xl font-bold leading-tight tracking-tighter ">
+                    <span>Znajdź</span>{" "}
+                    <span className="text-primary">uczelnię</span>{" "}
+                    <span>dla siebie</span>
+                </h1>
+
+                <div className="flex gap-6">
+                    <Button
+                        size={"lg"}
+                        className="flex gap-3 px-6 py-7 text-2xl font-medium"
+                        onClick={() => navigate(`/${language}/setup`)}
+                    >
+                        {authed ? "Kontynuuj" : "Rozpocznij"}
+                        <ArrowRightIcon className="w-6" />
+                    </Button>
+
+                    {!authed && (
+                        <Button
+                            size={"lg"}
+                            variant={"secondary"}
+                            className="flex gap-3 px-6 py-7 text-2xl font-medium"
+                            onClick={() => Cookies.remove("hackyeah")}
+                        >
+                            Nowy profil
+                            <ArrowPathIcon className="w-6" />
+                        </Button>
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
