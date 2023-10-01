9ebfd378ca603828070221bd6449108704df4d88
AreczeQ192 <askupien8@gmail.com>
2023-10-01 03:41:26 +0200

    StudiedStep

--- frontend/src/routes/setup/StudiedStep.tsx
+++ frontend/src/routes/setup/StudiedStep.tsx
@@ -10,40 +10,50 @@ export function StudiedStep(props: StepProps) {
     const [studied, setStudied] = useState("no");
 
     return (
-        <Card className="h-fit p-8">
-            <form
-                className="flex flex-col gap-4"
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    props.onSubmit({
-                        ...props.data,
-                        studyLevel: studied === "no" ? "1" : undefined,
-                    });
-                }}
-            >
-                <h1 className="text-center text-xl font-medium">
-                    Czy już studiowałeś/aś?
-                </h1>
-                <RadioGroup
-                    defaultValue={studied}
-                    value={studied}
-                    onValueChange={setStudied}
+        <div className="h-[calc(100vh - 2rem)] flex w-screen items-center justify-center sm:items-baseline">
+            <Card className="pb-6z h-[320px] w-[440px] p-8">
+                <form
+                    className="flex h-full flex-col gap-4"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        props.onSubmit({
+                            ...props.data,
+                            studyLevel: studied === "no" ? "1" : undefined,
+                        });
+                    }}
                 >
-                    <div className="flex items-center space-x-2">
-                        <RadioGroupItem value="yes" id="yes" />
-                        <Label htmlFor="yes">Tak</Label>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <RadioGroupItem value="no" id="no" />
-                        <Label htmlFor="no">Nie</Label>
-                    </div>
-                </RadioGroup>
+                    <h1 className="text-center text-2xl font-medium">
+                        Czy już studiowałeś/aś?
+                    </h1>
+                    <RadioGroup
+                        defaultValue={studied}
+                        value={studied}
+                        onValueChange={setStudied}
+                        className="h-full auto-rows-min gap-2 pt-4"
+                    >
+                        <div className="flex items-center space-x-2">
+                            <RadioGroupItem value="yes" id="yes" />
+                            <Label htmlFor="yes" className="text-lg">
+                                Tak
+                            </Label>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <RadioGroupItem value="no" id="no" />
+                            <Label htmlFor="no" className="text-lg">
+                                Nie
+                            </Label>
+                        </div>
+                    </RadioGroup>
 
-                <Button className="ml-auto flex gap-2 text-base" type="submit">
-                    Dalej
-                    <ArrowRightIcon className="w-4" />
-                </Button>
-            </form>
-        </Card>
+                    <Button
+                        className="ml-auto flex gap-2 text-base"
+                        type="submit"
+                    >
+                        Dalej
+                        <ArrowRightIcon className="w-4" />
+                    </Button>
+                </form>
+            </Card>
+        </div>
     );
 }
