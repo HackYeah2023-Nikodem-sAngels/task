d8451770dba044dfbffe8d513a57cf512110bdaf
git.nix <nikodemmarekit@gmail.com>
2023-10-01 03:59:01 +0200

    feat: get only embedings

--- embeding/main.py
+++ embeding/main.py
@@ -51,12 +51,21 @@ def embedding_from_string(
     return embedding_cache[(string, model)]
 
 # as an example, take the first description from the dataset
-example_string = df["description"].values[0]
-print(f"\nExample string: {example_string}")
+# example_string = "duya123"
+# print(f"\nExample string: {example_string}")
 
 # print the first 10 dimensions of the embedding
-example_embedding = embedding_from_string(example_string)
-print(f"\nExample embedding: {example_embedding[:10]}...")
+# z1 = embedding_from_string(example_string)
+# zall = pd.DataFrame([embedding_from_string(df["description"][0]), embedding_from_string(df["description"][1])])
+#
+# with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+#     print(zall)
+#
+# print(f"\nz1: {z1}...")
+# print(f"\nzall: {zall}...")
+# zall.to_csv('zall') 
+#
+# print("dot: ", zall.dot(z1))
 
 def print_recommendations_from_strings(
     strings,
@@ -67,40 +76,43 @@ def print_recommendations_from_strings(
     """Print out the k nearest neighbors of a given string."""
     # get embeddings for all strings
     embeddings = [embedding_from_string(string, model=model) for string in strings]
+    print(f"Embeddings: {[e[:4] for e in embeddings]}")
+
+
     # get the embedding of the source string
-    query_embedding = embeddings[index_of_source_string]
+    # query_embedding = embeddings[index_of_source_string]
     # get distances between the source embedding and other embeddings (function from embeddings_utils.py)
-    distances = distances_from_embeddings(query_embedding, embeddings, distance_metric="cosine")
+    # distances = distances_from_embeddings(query_embedding, embeddings, distance_metric="cosine")
     # get indices of nearest neighbors (function from embeddings_utils.py)
-    indices_of_nearest_neighbors = indices_of_nearest_neighbors_from_distances(distances)
+    # indices_of_nearest_neighbors = indices_of_nearest_neighbors_from_distances(distances)
 
     # print out source string
-    query_string = strings[index_of_source_string]
-    print(f"Source string: {query_string}")
-    # print out its k nearest neighbors
-    k_counter = 0
-    for i in indices_of_nearest_neighbors:
-        # skip any strings that are identical matches to the starting string
-        if query_string == strings[i]:
-            continue
-        # stop after printing out k articles
-        if k_counter >= k_nearest_neighbors:
-            break
-        k_counter += 1
+    # query_string = strings[index_of_source_string]
+    # print(f"Source string: {query_string}")
+    # # print out its k nearest neighbors
+    # k_counter = 0
+    # for i in indices_of_nearest_neighbors:
+    #     # skip any strings that are identical matches to the starting string
+    #     if query_string == strings[i]:
+    #         continue
+    #     # stop after printing out k articles
+    #     if k_counter >= k_nearest_neighbors:
+    #         break
+    #     k_counter += 1
+    #
+    #     # print out the similar strings and their distances
+    #     print(
+    #         f"""
+    #     --- Recommendation #{k_counter} (nearest neighbor {k_counter} of {k_nearest_neighbors}) ---
+    #     String: {strings[i]}
+    #     Distance: {distances[i]:0.3f}"""
+    #     )
 
-        # print out the similar strings and their distances
-        print(
-            f"""
-        --- Recommendation #{k_counter} (nearest neighbor {k_counter} of {k_nearest_neighbors}) ---
-        String: {strings[i]}
-        Distance: {distances[i]:0.3f}"""
-        )
-
-    return indices_of_nearest_neighbors
+    # return indices_of_nearest_neighbors
 
 article_descriptions = df["description"].tolist()
 
-tony_blair_articles = print_recommendations_from_strings(
+print_recommendations_from_strings(
     strings=article_descriptions,  # let's base similarity off of the article description
     index_of_source_string=0,  # let's look at articles similar to the first one about Tony Blair
     k_nearest_neighbors=5,  # let's look at the 5 most similar articles
